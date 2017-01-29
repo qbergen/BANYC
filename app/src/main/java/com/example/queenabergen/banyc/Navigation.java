37daf000ed9ce63dd4e10d6f210419a7af75f0d1
qbergen <queenabergen@gmail.com>
2017-01-29 13:18:05 -0500

    signup page layout and firebase code

--- app/src/main/java/com/example/queenabergen/banyc/Navigation.java
+++ app/src/main/java/com/example/queenabergen/banyc/Navigation.java
@@ -1,16 +1,15 @@
 package com.example.queenabergen.banyc;
 
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+public class Navigation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,3 +60,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 }
+
