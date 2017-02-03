b56f1341630f160ba95d3184a2d10a4e81eb4f90
qbergen <queenabergen@gmail.com>
2017-02-03 14:42:55 -0500

    clean landing page for merge

--- app/src/main/java/com/example/queenabergen/banyc/LandingPage.java
+++ app/src/main/java/com/example/queenabergen/banyc/LandingPage.java
@@ -2,16 +2,13 @@ package com.example.queenabergen.banyc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 public class LandingPage extends AppCompatActivity {
-    private ImageView drawerIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
-        drawerIcon = (ImageView)findViewById(R.id.youth_Icon);
 
     }
 
