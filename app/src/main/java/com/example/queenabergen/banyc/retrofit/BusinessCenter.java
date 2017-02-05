5154775c1659542d4141b351c468ba584d7bb788
hyunjkim <hkim6501@gmail.com>
2017-02-05 15:00:23 -0500

    Updated Facebook & Google

--- app/src/main/java/com/example/queenabergen/banyc/retrofit/BusinessCenter.java
+++ app/src/main/java/com/example/queenabergen/banyc/retrofit/BusinessCenter.java
@@ -28,14 +28,6 @@ public class BusinessCenter {
         description.add("Through a focus on business administration and entrepreneurship, students prepare for success at competitive colleges and universities.");
         description.add("In addition to the Stand Alone ESL program, our school offers a Bilingual Education Program in Spanish.");
         description.add("Students are exposed to the foundational aspects of business and teaching via their advisory class and are then placed in either the Education or the Business track.");
-        description.add("A program to develop the skills necessary for positions in the growing information technology and business industries. Students assume corporate roles and gain practical experience by applying skills and techniques learned in real world situations.");
-        description.add("Students learn about the arts through hands-on experience, written work and art creation. Field trips take advantage of the rich cultural environment of New York City. Students visit art galleries and museums and attend dramatic productions both on and off Broadway. Students also learn the business end of promoting music, drama, creative writing and visual arts through direct involvement in hands-on projects taking place at concerts, art galleries, poetry slams and play productions.");
-        description.add("College & career preparatory program for careers related to the business and finance sectors; curriculum developed by the National Academy Foundation (NAF); receives support from corporate partners.");
-        description.add("The Business and Technology Institute offers dedicated students a concentration in introductory business and computer science courses. This three year sequence includes project based explorations of business and management structures and an introduction to computer science. Included in this sequence are courses in web design, coding, programming and computer data anaylsis.");
-        description.add("An interdisciplinary program combining business and technology courses with a real hands-on working experience. The curriculum includes courses directed towards completing professional and academic certifications.");
-        description.add("Business courses including accounting, career and financial management, computer repair, entrepreneurship, and graphic arts");
-        description.add("Virtual Enterprise (VE) is a simulated business that is set up and run by students with the guidance of a teacher/facilitator and a business partner. Students will understand how employees, work-group teams and departments interact with each other and work together for the goal of the company.");
-        description.add("Accounting, Electronic Information Processing, Desktop Publishing, Web Page Design, e-commerce, Microsoft Office certification training.");
 
         initializeAdapter();
     }
