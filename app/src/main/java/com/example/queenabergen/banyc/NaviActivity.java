3ddbe8b793da0df1c53c6cd611ae04631e15ff66
hyunjkim <hkim6501@gmail.com>
2017-02-05 15:15:48 -0500

    Create Account

--- app/src/main/java/com/example/queenabergen/banyc/NaviActivity.java
+++ app/src/main/java/com/example/queenabergen/banyc/NaviActivity.java
@@ -3,7 +3,6 @@ package com.example.queenabergen.banyc;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,38 +10,19 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 import com.example.queenabergen.banyc.sportsresources.SportsRv;
 import com.example.queenabergen.banyc.subjects.business.BusinessPage;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 public class NaviActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private ViewFlipper flip;
-    private float initialX;
 
-    ImageView imageView;
-    private static final String TAG = " ";
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    public Button signInBTN;
-    public Button signupBTN;
-    public EditText emailInput;
-    public EditText passwordInput;
-    public ImageView facebookView;
-    public ImageView googleView;
-    public Animation animFadeOut;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -87,49 +67,6 @@ public class NaviActivity extends AppCompatActivity
         flip.startFlipping();
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void loginView() {
-        googleView = (ImageView) findViewById(R.id.google_btn);
-        passwordInput = (EditText) findViewById(R.id.password_input);
-        passwordInput.setElevation(50);
-        emailInput = (EditText) findViewById(R.id.email_input);
-        emailInput.setElevation(50);
-        signInBTN = (Button) findViewById(R.id.signin_btn);
-        signInBTN.setElevation(50);
-        signInBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                mAuthListener = new FirebaseAuth.AuthStateListener() {
-                    @Override
-                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                        FirebaseUser user = firebaseAuth.getCurrentUser();
-                        if (user != null) {
-                            // User is signed in
-                            Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                            Intent intent = new Intent(getApplicationContext(), LandingPage.class);
-                            startActivity(intent);
-                        } else {
-                            // User is signed out
-                            Log.d(TAG, "onAuthStateChanged:signed_out");
-                        }
-                        // ...
-                    }
-                };
-            }
-        });
-        signupBTN = (Button) findViewById(R.id.signup_btn);
-        signupBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.fade_out, R.anim.activityanim);
-
-            }
-        });
-
-    }
 
 
     @Override
@@ -178,6 +115,7 @@ public class NaviActivity extends AppCompatActivity
             Intent businessInten = new Intent(getApplicationContext(), BusinessPage.class);
             startActivity(businessInten);
         } else if (id == R.id.nav_stem) {
+
         } else if (id == R.id.nav_home) {
             Intent backHome = new Intent(getApplicationContext(), NaviActivity.class);
             startActivity(backHome);
